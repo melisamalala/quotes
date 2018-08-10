b87728a995227e82ba336a4ffdf4d49a6e1ef7f3
melisamalala <melissamalala@gmail.com>
2018-08-10 16:56:34 +0300

    Added Like and dislike buttons to jumbotrons

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,11 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
   styleUrls: ['./quote-details.component.css']
 })
-export class QuoteDetailsComponent implements OnInit {
+export class QuoteDetailsComponent {
+  numberOfLikes: number = 0;
+
+  likeButtonClick() {
+    this.numberOfLikes++;
+  }
+
+  dislikeButtonClick() {
+    this.numberOfLikes--;
+  }
 
   constructor() { }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-quote',
+//   templateUrl: './quote.component.html',
+//   styleUrls: ['./quote.component.css']
+// })
+// export class QuoteComponent implements OnInit {
+//
+//   quotes = [
+//   new Quote(1, 'Pineapples are yummy', 'Me', 'Anne', new Date(2018, 3, 1 )),
+//   new Quote(2, 'Be yourself; everyone else is eating pineapples.', 'Oscar Wilde',  'Myself', new Date(2012, 3, 9)),
+//   new Quote(3, 'So many pineapples, so little time.', 'Frank Ananas', 'Sami-Mai', new Date(2013, 1, 10)),
+//   new Quote(4, 'Be the pineapple that you wish to see in the world.', 'Mahatma Fruit', 'Hanna', new Date(2016, 7, 14)),
+//   new Quote(3, 'My pineapples know their limits, my mind refuses to accept them.', 'Unknown', 'Nanasi', new Date(2018, 4, 10)),
+// ];
+//
+//   addQuote(quote) {
+//         const quoteLength = this.quotes.length;
+//         quote.id = quoteLength + 1;
+//          quote.datePublished = new Date();
+//         this.quotes.push(quote);
+//
+//     }
+//
+//   toogleDetails(index) {
+//       this.quotes[index].showDetails = !this.quotes[index].showDetails;
+//   }
+//
+//   deleteQuote(isDelete, index) {
+//       if (isDelete) {
+//
+//         const toDelete = confirm(`Sure you wanna delete this, buddy?\n \n \" ${this.quotes[index].quote}\"`);
+//
+//
+//         if (toDelete) {
+//             this.quotes.splice(index, 1);
+//             }
+//         }
+//     }
+//
+//   constructor() { }
+//
+//   ngOnInit() {
+//   }
+//
+// }
